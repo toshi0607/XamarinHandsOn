22cb418cf1364cee3d1001bb6d40f6c0c7841f40
toshi0607 <s.toshi0607@gmail.com>
2016-11-20 12:55:01 +0900

    add project

---
+++ DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using DevDaysSpeakers.Model;
+using DevDaysSpeakers.Services;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace DevDaysSpeakers.ViewModel
+{
+    public class SpeakersViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Speaker> Speakers { get; set; }
+        public Command GetSpeakersCommand { get; set; }
+        public SpeakersViewModel()
+        {
+            Speakers = new ObservableCollection<Speaker>();
+            GetSpeakersCommand = new Command(
+                async () => await GetSpeakers(),
+                () => !IsBusy);
+        }
+
+        bool busy;
+        public bool IsBusy
+        {
+            get { return busy; }
+            set
+            {
+                busy = value;
+                OnPropertyChanged();
+                //Update the can execute
+                GetSpeakersCommand.ChangeCanExecute();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        async Task GetSpeakers()
+        {
+            if (IsBusy)
+                return;
+
+            Exception error = null;
+            try
+            {
+                IsBusy = true;
+                using (var client = new HttpClient())
+                {
+                    // サーバーから json を取得します
+                    var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+                    var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+                    Speakers.Clear();
+                    foreach (var item in items)
+                        Speakers.Add(item);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                error = ex;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+            if (error != null)
+                await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
+        }
+    }
+}
